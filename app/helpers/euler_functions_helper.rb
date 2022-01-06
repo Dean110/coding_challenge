c4a98237f68c11d9d4ec5823741a8300c4ff43d2
Ben Williams <ben.williams@joinroot.com>
2022-01-06 17:30:27 -0500

    rubocop clean up and failing spec update.

--- app/helpers/euler_functions_helper.rb
+++ app/helpers/euler_functions_helper.rb
@@ -1,52 +1,50 @@
+# frozen_string_literal: true
+
 module EulerFunctionsHelper
-require 'prime' 
+  require 'prime'
 
-    # https://projecteuler.net/problem=1
-    def euler_one(number_to_act_on)
-        count = 0 
-        
-        for i in 3..number_to_act_on - 1 do
-            count += i if (i % 3 == 0 || i % 5 == 0)
-        end
+  # https://projecteuler.net/problem=1
+  def euler_one(number_to_act_on)
+    count = 0
 
-        count
+    (3..number_to_act_on - 1).each do |i|
+      count += i if (i % 3).zero? || (i % 5).zero?
     end
-    # https://projecteuler.net/problem=2
-    def euler_two(number_not_to_exceed)
 
-        sum = 0
-        return sum if number_not_to_exceed < 3
-        
-        i=3
-        sum = 2
-        first_value = 2
+    count
+  end
 
-        while i < number_not_to_exceed
-            sum += i if i % 2 == 0   
-            next_value = first_value + i
-            first_value = i
-            i = next_value
-            puts next_value
-        end
+  # https://projecteuler.net/problem=2
+  def euler_two(number_not_to_exceed)
+    sum = 0
+    return sum if number_not_to_exceed < 3
 
-        sum
+    i = 3
+    sum = 2
+    first_value = 2
+
+    while i < number_not_to_exceed
+      sum += i if i.even?
+      next_value = first_value + i
+      first_value = i
+      i = next_value
+      puts next_value
     end
-       
-    # https://projecteuler.net/problem=3
-    def euler_three(number_to_factor)
 
-        return number_to_factor if number_to_factor < 4
+    sum
+  end
 
-        factors = Array.new
+  # https://projecteuler.net/problem=3
+  def euler_three(number_to_factor)
+    return number_to_factor if number_to_factor < 4
 
-        i = 2
-        while i <= number_to_factor
-            if number_to_factor % i == 0 &&  Prime.prime?(i)
-                factors.append(i) 
-            end
-            i = i + 1
-        end
-        return factors.max()
-        
+    factors = []
+
+    i = 2
+    while i <= number_to_factor
+      factors.append(i) if (number_to_factor % i).zero? && Prime.prime?(i)
+      i += 1
     end
+    factors.max
+  end
 end
--- spec/helpers/euler_functions_helper_spec.rb
+++ spec/helpers/euler_functions_helper_spec.rb
@@ -1,133 +1,127 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'prime'
 
-# Specs in this file have access to a helper object that includes
-# the EulerFunctionsHelper. For example:
-#
-# describe EulerFunctionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe EulerFunctionsHelper, type: :helper do
-  describe "Summing the numbers that are have factors of 3 or 5 below a given number." do
-    
-    it "given 0, returns 0" do
-      expect(helper.euler_one 0).to eq(0)
+  describe 'Summing the numbers that are have factors of 3 or 5 below a given number.' do
+    it 'given 0, returns 0' do
+      expect(helper.euler_one(0)).to eq(0)
     end
 
-    it "given 1, returns 1" do
-      expect(helper.euler_one 1).to eq(0)
-    end
-    
-    it "given 3, returns 0" do
-        expect(helper.euler_one 3).to eq(0)
+    it 'given 1, returns 1' do
+      expect(helper.euler_one(1)).to eq(0)
     end
 
-    it "given 4, returns 3" do
-      expect(helper.euler_one 4).to eq(3)
+    it 'given 3, returns 0' do
+      expect(helper.euler_one(3)).to eq(0)
     end
 
-    it "given 6, returns 8" do
-      expect(helper.euler_one 6).to eq(8)
+    it 'given 4, returns 3' do
+      expect(helper.euler_one(4)).to eq(3)
     end
-    it "given 10, returns 23" do
-    expect(helper.euler_one 10).to eq(23)
+
+    it 'given 6, returns 8' do
+      expect(helper.euler_one(6)).to eq(8)
+    end
+    it 'given 10, returns 23' do
+      expect(helper.euler_one(10)).to eq(23)
     end
   end
 
-  describe "Summing the even numbers in the Fibbonaci sequence below a given number" do
-  
-    it "given 0, returns 0" do
-      expect(helper.euler_two 0).to eq(0)
+  describe 'Summing the even numbers in the Fibbonaci sequence below a given number' do
+    it 'given 0, returns 0' do
+      expect(helper.euler_two(0)).to eq(0)
     end
 
-    it "given 1, returns 0" do
-      expect(helper.euler_two 1).to eq(0)
+    it 'given 1, returns 0' do
+      expect(helper.euler_two(1)).to eq(0)
     end
 
-    it "given 2, returns 0" do
-      expect(helper.euler_two 2).to eq(0)
+    it 'given 2, returns 0' do
+      expect(helper.euler_two(2)).to eq(0)
     end
 
-    it "given 3, returns 2" do
-      expect(helper.euler_two 3).to eq(2)
+    it 'given 3, returns 2' do
+      expect(helper.euler_two(3)).to eq(2)
     end
 
-    it "given 4, returns 2" do
-      expect(helper.euler_two 4).to eq(2)
+    it 'given 4, returns 2' do
+      expect(helper.euler_two(4)).to eq(2)
     end
 
-    it "given 5, returns 2" do
-      expect(helper.euler_two 5).to eq(2)
+    it 'given 5, returns 2' do
+      expect(helper.euler_two(5)).to eq(2)
     end
 
-    it "given 6, returns 2" do
-      expect(helper.euler_two 6).to eq(2)
+    it 'given 6, returns 2' do
+      expect(helper.euler_two(6)).to eq(2)
     end
 
-    it "given 7, returns 2" do
-      expect(helper.euler_two 7).to eq(2)
+    it 'given 7, returns 2' do
+      expect(helper.euler_two(7)).to eq(2)
     end
 
-    it "given 8, returns 2" do
-      expect(helper.euler_two 8).to eq(2)
+    it 'given 8, returns 2' do
+      expect(helper.euler_two(8)).to eq(2)
     end
 
-    it "given 9, returns 10" do
-      expect(helper.euler_two 9).to eq(10)
+    it 'given 9, returns 10' do
+      expect(helper.euler_two(9)).to eq(10)
     end
-    
-    it "given 100, returns 10" do
-      expect(helper.euler_two 100).to eq(10)
+
+    it 'given 100, returns 10' do
+      expect(helper.euler_two(100)).to eq(2 + 8 + 34)
     end
   end
 
-  describe "finding the largest prime factor for a very large number" do
-    it "given 1, returns 1" do
-      expect(helper.euler_three 1).to eq(1)
+  describe 'finding the largest prime factor for a very large number' do
+    it 'given 1, returns 1' do
+      expect(helper.euler_three(1)).to eq(1)
     end
 
-    it "given 2, returns 2" do
-      expect(helper.euler_three 2).to eq(2)
-    end
-    
-    it "given 4, returns 2" do 
-      expect(helper.euler_three 4).to eq(2)
+    it 'given 2, returns 2' do
+      expect(helper.euler_three(2)).to eq(2)
     end
 
-    it "given 5, returns 5" do
-      expect(helper.euler_three 5).to eq(5)
+    it 'given 4, returns 2' do
+      expect(helper.euler_three(4)).to eq(2)
     end
 
-    it "given 6, returns 3" do
-      expect(helper.euler_three 6).to eq(3)
+    it 'given 5, returns 5' do
+      expect(helper.euler_three(5)).to eq(5)
     end
 
-    it "given 7, returns 7" do
-      expect(helper.euler_three 7).to eq(7)
-    end
-    
-    it "given 8, returns 2" do
-      expect(helper.euler_three 8).to eq(2)
+    it 'given 6, returns 3' do
+      expect(helper.euler_three(6)).to eq(3)
     end
 
-    it "given 9, returns 3" do
-      expect(helper.euler_three 9).to eq(3)
-    end
-  
-    it "given 10, returns 5" do
-      expect(helper.euler_three 10).to eq(5)
+    it 'given 7, returns 7' do
+      expect(helper.euler_three(7)).to eq(7)
     end
 
-    it "given 11, returns 11" do
-      expect(helper.euler_three 11).to eq(11)
+    it 'given 8, returns 2' do
+      expect(helper.euler_three(8)).to eq(2)
     end
 
-    it "given 13195, returns 29" do
-      expect(helper.euler_three 13195).to eq(29)
+    it 'given 9, returns 3' do
+      expect(helper.euler_three(9)).to eq(3)
+    end
+
+    it 'given 10, returns 5' do
+      expect(helper.euler_three(10)).to eq(5)
+    end
+
+    it 'given 11, returns 11' do
+      expect(helper.euler_three(11)).to eq(11)
+    end
+
+    it 'given 13195, returns 29' do
+      expect(helper.euler_three(13_195)).to eq(29)
+    end
+
+    it 'given 21023, returns 21023' do
+      expect(helper.euler_three(21_023)).to eq(21_023)
     end
   end
 end
